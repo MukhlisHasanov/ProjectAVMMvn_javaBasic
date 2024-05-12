926b467af0f597033d77fabf7ab54310572953ac
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-05-12 18:36:50 +0200

    12.05.24

--- src/main/java/avm/Avm.java
+++ src/main/java/avm/Avm.java
@@ -3,11 +3,14 @@ package avm;
 import avm.controller.AvmController;
 import avm.controller.ClientController;
 import avm.products.Client;
+import avm.products.ClothProduct;
 import avm.repository.*;
 import avm.service.CafeService;
 import avm.service.CinemaService;
 import avm.service.ClothService;
 import avm.service.MarketService;
+
+import java.sql.SQLException;
 
 /**
  * AIT-TR, Cohort 42.1, Java Basic, Project #2 AVM
@@ -15,27 +18,30 @@ import avm.service.MarketService;
  * @version Apr-2024
  */
 public class Avm {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        final String SQLITE_DB_AVM = "jdbc:sqlite:C:/temp/AvmDB.db";
+
         ClientRepository clientRepository = new ClientRepository();
         ClientController clientController = new ClientController(clientRepository);
         clientRepository.initClient();
         Client client = clientController.start();
 
-        MarketRepository marketRepository = new MarketRepository();
-        ClothRepository clothRepository = new ClothRepository();
-        MovieRepository movieRepository = new MovieRepository();
-        CafeRepository cafeRepository = new CafeRepository();
+        MarketRepository marketRepository = new MarketRepository(SQLITE_DB_AVM);
+        ClothRepository clothRepository = new ClothRepository(SQLITE_DB_AVM);
+        MovieRepository movieRepository = new MovieRepository(SQLITE_DB_AVM);
+        CafeRepository cafeRepository = new CafeRepository(SQLITE_DB_AVM);
 
         MarketService marketService = new MarketService(client,marketRepository);
         ClothService clothService = new ClothService(client, clothRepository);
         CinemaService cinemaService = new CinemaService(client, movieRepository);
         CafeService cafeService = new CafeService(client,cafeRepository);
 
-        marketRepository.initMarket();
-        clothRepository.initCloth();
-        movieRepository.initMovie();
-        cafeRepository.initCafe();
+//        marketRepository.initMarket();
+//        clothRepository.initCloth();
+//        movieRepository.initMovie();
+//        cafeRepository.initCafe();
 
-        new AvmController(marketService, clothService, cinemaService,cafeService).run();
+        new AvmController(marketService, clothService, cinemaService,cafeService, client).run();
     }
 }
