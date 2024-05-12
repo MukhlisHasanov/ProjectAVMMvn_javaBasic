f1847667d92ddcb4e7be5c673d22285d14beee98
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-05-12 21:52:44 +0200

    Client

--- src/main/java/avm/products/Client.java
+++ src/main/java/avm/products/Client.java
@@ -6,14 +6,16 @@ package avm.products;
  * @version Apr-2024
  */
 public class Client {
+    float wallet;
     int id, age;
     String name;
     private static int idCounter = 0;
 
-    public Client(String name, int age) {
+    public Client(String name, int age, float wallet) {
         this.id = ++idCounter;
         this.name = name;
         this.age = age;
+        this.wallet = wallet;
     }
 
     public String getName() {
@@ -28,18 +30,19 @@ public class Client {
         return id;
     }
 
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
+    public float getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(float wallet) {
+        this.wallet = wallet;
+    }
 
     @Override
     public String toString() {
         return  "[" + name +
                 ", id: " + id +
-                ", age: " + age + "]";
+                ", age: " + age +
+                ", wallet: " + wallet +"]";
     }
 }
