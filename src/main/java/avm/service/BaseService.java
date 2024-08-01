c3e7e9eb7e457fb478eefa3f3c70b9b393bb03b1
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2024-08-02 00:27:54 +0200

    Update BaseService.java

--- src/main/java/avm/service/BaseService.java
+++ src/main/java/avm/service/BaseService.java
@@ -6,7 +6,6 @@ import avm.repository.ProductRepository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
